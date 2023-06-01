e377c6be7e4cab809a18cb51596a8f7e294a4f29
Marcelee Laylay <youmustbeajoke@gmail.com>
2023-06-01 22:46:19 +0800

    Added the dynamic and incremental static regeneration page

--- src/app/dynamic/page.tsx
+++ src/app/dynamic/page.tsx
@@ -1,8 +1,56 @@
+import { UnsplashImage } from "@/models/unplash-image";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata = {
+  title: "Image Gallery | Dynamic Picture",
+};
+
+// This one makes it dynamic because it revalidates it for 0 seconds.
+// Meaning that it would change it every time the page has been refreshed
+export const revalidate = 0;
+
 export default async function DynamicPage() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  //prettier-ignore
+  const response = await fetch("https://api.unsplash.com/photos/random?client_id=" + process.env.UNSPLASH_ACCESS_KEY, {
+    /*
+    They do the same thing as the (revalidate = 0;)
+    next: {
+      revalidate: 0
+    },
+    cache: "no-cache"
+    */
+  });
+  const image: UnsplashImage = await response.json();
+
+  const width = Math.min(500, image.width);
+  const height = (width / image.width) * image.height;
+
   return (
     <>
-      <div>Hello I am from the dynamic page</div>
+      <div className="flex flex-col items-center gap-2">
+        <div className="min-w-fit max-w-sm bg-slate-500 text-slate-100 sm:text-xs py-2 px-3 shadow-lg rounded">
+          This page
+          <strong> fetches data dynamically</strong> and every time you refresh
+          the page, you get a new image from Unsplash.
+        </div>
+        <Image
+          src={image.urls.raw}
+          alt={image.description}
+          width={width}
+          height={height}
+          className="shadow-lg rounded"
+        ></Image>
+        <div className="w-fit text-sm flex gap-1">
+          by
+          <Link
+            href={`/users/${image.user.username}`}
+            className="font-bold hover:underline hover:text-slate-700"
+          >
+            {image.user.username}
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
--- src/app/isr/page.tsx
+++ src/app/isr/page.tsx
@@ -1,8 +1,57 @@
-export default async function IsrPage() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+import { UnsplashImage } from "@/models/unplash-image";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata = {
+  title: "Image Gallery | Incremental Static Regeneration",
+};
+
+// This one makes it also dynamic because it revalidates or change the picture after 15 seconds.
+// Of course it will still cache it, but after 15 seconds it will get a new picture then
+// the process repeats.
+export const revalidate = 15;
+
+export default async function ISRPage() {
+  //prettier-ignore
+  const response = await fetch("https://api.unsplash.com/photos/random?client_id=" + process.env.UNSPLASH_ACCESS_KEY, {
+    /*
+    They do the same thing as the (revalidate = 15;)
+    next: {
+      revalidate: 15
+    },
+    */
+  });
+  const image: UnsplashImage = await response.json();
+
+  const width = Math.min(500, image.width);
+  const height = (width / image.width) * image.height;
+
   return (
     <>
-      <div>Hello I am from the isr page</div>
+      <div className="flex flex-col items-center gap-2">
+        <div className="min-w-fit max-w-sm bg-slate-500 text-slate-100 sm:text-xs py-2 px-3 shadow-lg rounded">
+          This page uses
+          <strong> incremental static regeneration</strong>. Meaning that it
+          will cache the image for 15 seconds. After that it will re-fetch
+          another image if you ever refresh the page.
+        </div>
+        <Image
+          src={image.urls.raw}
+          alt={image.description}
+          width={width}
+          height={height}
+          className="shadow-lg rounded"
+        ></Image>
+        <div className="w-fit text-sm flex gap-1">
+          by
+          <Link
+            href={`/users/${image.user.username}`}
+            className="font-bold hover:underline hover:text-slate-700"
+          >
+            {image.user.username}
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
